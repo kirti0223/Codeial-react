e6dac386d91da579e94abbd77018c47991e17c4d
kirti0223 <kirtisingla58@gmail.com>
2021-09-25 18:30:53 +0530

    Posts list added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <div className="App">App</div>;
+import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
